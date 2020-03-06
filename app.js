25b9258fae4bac050aa7c305bea8fffab174d2fb
Omar <omarreda5050@gmail.om>
2020-03-06 21:33:33 +0200

    Database

--- app.js
+++ app.js
@@ -1,86 +1,67 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var request = require("request");
-var mongoose = require("mongoose");
-var app = express();
+//////////////////////////////////////////
+/// Variables
+//////////////////////////////////////////
+
+var express = require("express"),
+  bodyParser = require("body-parser"),
+  request = require("request"),
+  mongoose = require("mongoose"),
+  app = express();
 
 // app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/yelp_camp");
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
-mongoose.connect("mongodb://localhost/campgrounds");
 
-var campSchema = new mongoose.Schema({
+//////////////////////////////////////////
+/// Schema Setup & Model
+//////////////////////////////////////////
+
+var campgroundSchema = new mongoose.Schema({
   name: String,
   image: String
 });
 
-var camp = mongoose.model("Camp", campSchema);
+var Campground = mongoose.model("Campground", campgroundSchema);
 
-var campgrounds = [
-  {
-    name: "Salmon Greek",
-    image:
-      "https://negromanosphere.com/wp-content/uploads/2018/02/camping-2581242_960_720-678x381.jpg"
-  },
-  {
-    name: "Country Hills",
-    image:
-      "http://www.robertgaudette.com/wp-content/uploads/2019/06/going-camping-2.jpg"
-  },
-  {
-    name: "Mountain Ghost Rest",
-    image:
-      "https://negromanosphere.com/wp-content/uploads/2018/02/camping-2581242_960_720-678x381.jpg"
-  },
-  {
-    name: "Salmon Greek",
-    image:
-      "https://negromanosphere.com/wp-content/uploads/2018/02/camping-2581242_960_720-678x381.jpg"
-  },
-  {
-    name: "Country Hills",
-    image:
-      "http://www.robertgaudette.com/wp-content/uploads/2019/06/going-camping-2.jpg"
-  },
-  {
-    name: "Mountain Ghost Rest",
-    image:
-      "https://negromanosphere.com/wp-content/uploads/2018/02/camping-2581242_960_720-678x381.jpg"
-  },
-  {
-    name: "Salmon Greek",
-    image:
-      "https://negromanosphere.com/wp-content/uploads/2018/02/camping-2581242_960_720-678x381.jpg"
-  }
-];
-
-//////////////////////////////////////////////
+//////////////////////////////////////////
 /// Routes
-//////////////////////////////////////////////
+//////////////////////////////////////////
 
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
 app.get("/campgrounds", function(req, res) {
-  res.render("campgrounds", { campgrounds: campgrounds });
+  Campground.find({}, function(err, allCampgrounds) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("campgrounds", { campgrounds: allCampgrounds });
+    }
+  });
 });
 
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
   var newCampground = { name: name, image: image };
-  campgrounds.push(newCampground);
-  res.redirect("/campgrounds");
+  Campground.create(newCampground, function(err, newCreated) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.redirect("/campgrounds");
+    }
+  });
 });
 
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");
 });
 
-///////////////////////////////////////////////
+//////////////////////////////////////////
 /// Server
-///////////////////////////////////////////////
+//////////////////////////////////////////
 
 app.listen(3000, function() {
   console.log("Assignment Server 3000");
