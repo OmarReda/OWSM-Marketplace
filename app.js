1dd57e8dc17af92fae84925dc47721da56c91ac4
Omar <omarreda5050@gmail.om>
2020-03-07 18:32:59 +0200

    Item page and RESTful Routes

--- app.js
+++ app.js
@@ -19,33 +19,54 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 var campgroundSchema = new mongoose.Schema({
   name: String,
-  image: String
+  image: String,
+  description: String
 });
 
 var Campground = mongoose.model("Campground", campgroundSchema);
+
+// Campground.create(
+//   {
+//     name: "Grantie Hill",
+//     image:
+//       "https://pixabay.com/get/52e8d4444255ae14f6da8c7dda793f7f1636dfe2564c704c7d2c7ed29344c659_340.jpg",
+//     description: "This is one of the best campgrounds ever."
+//   },
+//   function(err, campground) {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log(campground);
+//     }
+//   }
+// );
 
 //////////////////////////////////////////
 /// Routes
 //////////////////////////////////////////
 
+// SHOW - show the landing page
 app.get("/", function(req, res) {
   res.render("landing");
 });
 
+//INDEX - Show all the campgrounds in the database
 app.get("/campgrounds", function(req, res) {
   Campground.find({}, function(err, allCampgrounds) {
     if (err) {
       console.log(err);
     } else {
-      res.render("campgrounds", { campgrounds: allCampgrounds });
+      res.render("index", { campgrounds: allCampgrounds });
     }
   });
 });
 
+//CREATE - create a new campground
 app.post("/campgrounds", function(req, res) {
   var name = req.body.name;
   var image = req.body.image;
-  var newCampground = { name: name, image: image };
+  var desc = req.body.description;
+  var newCampground = { name: name, image: image, description: desc };
   Campground.create(newCampground, function(err, newCreated) {
     if (err) {
       console.log(err);
@@ -55,8 +76,20 @@ app.post("/campgrounds", function(req, res) {
   });
 });
 
+// NEW - show the form to create a new campground
 app.get("/campgrounds/new", function(req, res) {
   res.render("new");
+});
+
+// SHOW - show the details of single campground
+app.get("/campgrounds/:id", function(req, res) {
+  Campground.findById(req.params.id, function(err, foundCampground) {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("show", { campground: foundCampground });
+    }
+  });
 });
 
 //////////////////////////////////////////
